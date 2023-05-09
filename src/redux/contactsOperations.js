67e8e16d8ac62989a3d2b3a6dd20a1c5e496c7fb
Daniil <82281229+SweetnessDark@users.noreply.github.com>
2023-05-09 10:14:56 +0300

    bug fix

--- src/redux/contactsOperations.js
+++ src/redux/contactsOperations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://6454c278f803f34576307ca9.mockapi.io';
+axios.defaults.baseURL = 'https://6459edb895624ceb21f2af93.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContact',
